6b818ac3bd35620cbb3f5cb2f7c1a412a5b9cefb
Tanuki Fur Hire <xy4nid3@gmail.com>
2026-09-04 10:55:56 +0800

    Initial Commit

---
+++ client.c
@@ -0,0 +1,429 @@
+/* gcc client.c messages.c -o client $(pkg-config --cflags --libs glfw3) -lGL $(pkg-config --cflags --libs CycloneDDS) -pthread -lm*/
+
+/* Dr Khoo Teck Ping
+27 Aug 2026
+1. DDS client
+2. Supports max 5 clients
+3. Start server, then start client one by one
+4. Each client can see itself and all other clients
+5. Each client can move itself using the arrow keys
+6. Collision detection among all players and the window is supported */
+
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include "dds/dds.h"
+#include "messages.h"
+#include "stb_easy_font.h"
+
+#define STB_EASY_FONT_IMPLEMENTATION
+
+#define MAX_SAMPLES 1
+#define MAX_PLAYERS 5
+
+
+#define WIDTH  800
+#define HEIGHT 600
+#define SQ_WIDTH 50
+
+
+// Player coord defined as NW corner of square
+typedef struct
+{
+    bool b_active;
+    int x;
+    int y;
+} Player;
+
+// ### GLOBAL VARIABLES ###
+int int_player_id = -1;
+
+// Table to track clients
+// Init all members to 0
+static Player arr_Players[MAX_PLAYERS] = {0};
+
+// Direction - n means no direction
+char ch_dir = 'n';
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void draw_text(float x, float y, float scale, const char *text)
+{
+    char buffer[4096];
+
+    int num_quads = stb_easy_font_print(
+        0, 0,
+        (char *)text,
+        NULL,
+        buffer,
+        sizeof(buffer)
+    );
+
+    glPushMatrix();
+
+    glTranslatef(x, y, 0.0f);
+    glScalef(scale, scale, 1.0f);
+
+    glEnableClientState(GL_VERTEX_ARRAY);
+
+    glVertexPointer(2, GL_FLOAT, 16, buffer);
+    glDrawArrays(GL_QUADS, 0, num_quads * 4);
+
+    glDisableClientState(GL_VERTEX_ARRAY);
+
+    glPopMatrix();
+}
+
+void *worker(void *arg)
+{
+	dds_entity_t participant;
+	dds_entity_t join_topic;
+	dds_entity_t response_topic;
+	dds_entity_t input_topic;
+	dds_entity_t position_topic;
+	dds_entity_t response_reader;
+	dds_entity_t position_reader;
+	dds_entity_t join_writer;
+	dds_entity_t input_writer;
+	
+	//Pointer to unknown type
+	void *samples_response[MAX_SAMPLES];
+	dds_sample_info_t infos_response[MAX_SAMPLES];
+	void *samples_position[MAX_SAMPLES];
+	dds_sample_info_t infos_position[MAX_SAMPLES];
+	dds_return_t rc;
+	
+	/* ----------------------------------------------------- */
+    /* Create participant                                    */
+    /* ----------------------------------------------------- */
+
+    participant = dds_create_participant
+	(
+        DDS_DOMAIN_DEFAULT,
+        NULL,
+        NULL
+    );
+
+    if (participant < 0)
+    {
+        printf("Failed to create participant\n");
+        //return 1;
+    }
+
+    /* ----------------------------------------------------- */
+    /* Create topics                                          */
+    /* ----------------------------------------------------- */
+
+    join_topic = dds_create_topic
+	(
+        participant,
+        &JoinRequest_desc,
+        "JoinRequest",
+        NULL,
+        NULL
+    );
+	
+	response_topic = dds_create_topic
+	(
+        participant,
+        &JoinResponse_desc,
+        "JoinResponse",
+        NULL,
+        NULL
+    );
+	
+	input_topic = dds_create_topic
+	(
+        participant,
+        &Input_desc,
+        "Input",
+        NULL,
+        NULL
+    );
+	
+	position_topic = dds_create_topic
+	(
+        participant,
+        &Position_desc,
+        "Position",
+        NULL,
+        NULL
+    );
+	
+	/* ----------------------------------------------------- */
+    /* Create writers                                         */
+    /* ----------------------------------------------------- */
+
+    join_writer = dds_create_writer(
+        participant,
+        join_topic,
+        NULL,
+        NULL
+    );
+	
+	input_writer = dds_create_writer(
+        participant,
+        input_topic,
+        NULL,
+        NULL
+    );
+	
+	/* ----------------------------------------------------- */
+    /* Create readers                                         */
+    /* ----------------------------------------------------- */
+
+    response_reader = dds_create_reader(
+        participant,
+        response_topic,
+        NULL,
+        NULL
+    );
+	
+	position_reader = dds_create_reader(
+        participant,
+        position_topic,
+        NULL,
+        NULL
+    );
+		
+	JoinRequest request;
+    request.b_dummy = true;
+	
+	/* Initialize sample buffer, by pointing the void pointer within
+	* the buffer array to a valid sample memory location. */
+	samples_response[0] = JoinResponse__alloc();
+	
+	samples_position[0] = Position__alloc();
+	
+	// Record the time now
+	uint64_t last_join_req_time = dds_time();
+	/* printf("last_join_req_time = %" PRIu64 "\n", last_join_req_time); */
+	while (1)
+	{
+		// If not in a game yet
+		pthread_mutex_lock(&mutex);
+		int int_player_id_temp = int_player_id;
+		pthread_mutex_unlock(&mutex);
+		if (int_player_id_temp == -1)
+		{
+			//Listen for server join response
+			rc = dds_take(response_reader, samples_response, infos_response, MAX_SAMPLES, MAX_SAMPLES);
+			
+			if (rc < 0)
+				DDS_FATAL("dds_take: %s\n", dds_strretcode(-rc));
+
+			/* Check if we read some data and it is valid. */
+			if ((rc > 0) && (infos_response[0].valid_data))
+			{
+				printf("Join response received\n");
+				JoinResponse *response = (JoinResponse *) samples_response[0];
+				int int_resp_player_id = response->int_player_id;
+				// If server responded with an invalid player ID
+				if (int_resp_player_id == 0)
+					printf("Unable to join\n");
+				else
+				{
+					// printf("Join response player ID = %d\n", response->int_player_id);
+					pthread_mutex_lock(&mutex);
+					// Update player ID
+					int_player_id = int_resp_player_id;
+					pthread_mutex_unlock(&mutex);
+				}
+			}
+			else
+			{
+				/* Polling sleep. */
+				dds_sleepfor (DDS_MSECS (20));
+			}
+			
+			// Send request every 1s
+			// Get the current time
+			uint64_t curr_time = dds_time();
+			/* printf("curr_time = %" PRIu64 "\n", curr_time); */
+			// Get the time difference
+			if (curr_time - last_join_req_time > DDS_SECS(1))
+			{
+				// Update the last req time
+				last_join_req_time = curr_time;
+				
+				// Send join request
+				rc = dds_write(join_writer, &request);
+				printf("Join request sent\n");
+				if (rc != DDS_RETCODE_OK)
+				{
+					DDS_FATAL("dds_write: %s\n", dds_strretcode(-rc));
+				}
+			}
+		}
+		
+		// Client is an active player
+		else
+		{
+			pthread_mutex_lock(&mutex);
+			char ch_dir_temp = ch_dir;
+			pthread_mutex_unlock(&mutex);
+			// If there is some direction input
+			if(ch_dir_temp != 'n')
+			{
+				// Create a fresh input
+				Input input;
+				input.int_player_id = int_player_id;
+				
+				// Set the direction in the message
+				input.ch_direction = ch_dir_temp;			
+				// Send input
+				rc = dds_write(input_writer, &input);
+				// printf("curr_time = %" PRIu64 "\n", dds_time());
+				if (rc != DDS_RETCODE_OK)
+				{
+					DDS_FATAL("dds_write: %s\n", dds_strretcode(-rc));
+				}
+				printf("Input sent\n");
+				// Need to slow down how often input gets sent, otherwise, 
+				//the player will move to the window limits immediately
+				dds_sleepfor(DDS_MSECS(10));
+			}
+		}
+
+		// Listen for position
+		rc = dds_take(position_reader, samples_position, infos_position, MAX_SAMPLES, MAX_SAMPLES);
+		
+		if (rc < 0)
+			DDS_FATAL("dds_take: %s\n", dds_strretcode(-rc));
+
+		// Check if we read some data and it is valid.
+		if ((rc > 0) && (infos_position[0].valid_data))
+		{
+			printf("Position received\n");
+			Position *position = (Position *) samples_position[0];
+			// Get the player ID
+			int int_position_player_id = position->int_player_id;			
+			pthread_mutex_lock(&mutex);
+			// Set this player to active
+			arr_Players[int_position_player_id-1].b_active = true;
+			// Set this player's x
+			arr_Players[int_position_player_id-1].x = position->int_x;
+			// Set this player's y
+			arr_Players[int_position_player_id-1].y = position->int_y;
+			pthread_mutex_unlock(&mutex);
+		}
+	}
+}
+
+int main(void)
+{
+	pthread_t thread;
+
+    if (pthread_create(&thread, NULL, worker, NULL) != 0)
+    {
+        perror("pthread_create");
+        return 1;
+    }
+	
+	
+    if (!glfwInit())
+        return 1;
+
+    GLFWwindow *window =
+        glfwCreateWindow(WIDTH, HEIGHT, "DDS Client (Click within the window to control)", NULL, NULL);
+
+    if (!window)
+    {
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    /* Use pixel coordinates */
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    /* glOrtho(0, WIDTH, 0, HEIGHT, -1, 1); */
+	glOrtho(0, WIDTH, HEIGHT, 0, -1, 1);
+
+  /*   glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity(); */
+
+    while (!glfwWindowShouldClose(window))
+    {
+		 /* Process keyboard/window events */
+        glfwPollEvents();
+
+        /* Non-blocking keyboard input */
+        if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+		{
+			pthread_mutex_lock(&mutex);
+			ch_dir = 'u';
+			pthread_mutex_unlock(&mutex);
+		}
+		
+        else if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+		{
+			pthread_mutex_lock(&mutex);
+			ch_dir = 'd';
+			pthread_mutex_unlock(&mutex);
+		}
+		
+		else if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+		{
+			pthread_mutex_lock(&mutex);
+			ch_dir = 'l';
+			pthread_mutex_unlock(&mutex);
+		}
+		
+		else if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+		{
+			pthread_mutex_lock(&mutex);
+			ch_dir = 'r';
+			pthread_mutex_unlock(&mutex);
+		}
+		
+		else
+		{
+			pthread_mutex_lock(&mutex);
+			ch_dir = 'n';
+			pthread_mutex_unlock(&mutex);
+		}
+		
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+		glColor3f(1.0f, 0.0f, 0.0f);
+		
+		
+		char str_banner[100] = "Waiting for server ...";
+		if (int_player_id != -1)
+		{
+			snprintf(str_banner, sizeof(str_banner), "Player %d", int_player_id);
+		}
+		draw_text(320, 20, 3.0f, str_banner);
+		
+		for(int i = 0; i < MAX_PLAYERS; i++)
+		{
+			pthread_mutex_lock(&mutex);
+			if(arr_Players[i].b_active == true)
+			{
+				// Label the player ID
+				char str_player[2] = "";
+				snprintf(str_player, sizeof(str_player), "%d", i+1);
+				/* printf("Coords = %d, %d\n",arr_Players[i].x, arr_Players[i].y);				 */
+				/* draw_text(100, 400, 3.0f, str_banner); */
+				
+				draw_text(arr_Players[i].x, arr_Players[i].y, 3.0f, str_player);				
+				
+				glBegin(GL_LINE_LOOP);
+					glVertex2f(arr_Players[i].x, arr_Players[i].y);
+					glVertex2f(arr_Players[i].x, arr_Players[i].y + SQ_WIDTH);
+					glVertex2f(arr_Players[i].x + SQ_WIDTH, arr_Players[i].y + SQ_WIDTH);
+					glVertex2f(arr_Players[i].x + SQ_WIDTH, arr_Players[i].y);
+				glEnd();
+			}
+			pthread_mutex_unlock(&mutex);			
+		}
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,461 @@
+/* gcc -o server server.c messages.c $(pkg-config --cflags --libs CycloneDDS) */
+
+/* Dr Khoo Teck Ping
+27 Aug 2026
+1. DDS server
+2. Supports max 5 clients
+3. Start this server, then start client one by one
+4. Each client can see itself and all other clients
+5. Each client can move itself using the arrow keys
+6. Collision detection among all players and the window is supported */
+
+/* mutex lock/unlock must be matched
+watch out for break out of loop */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <time.h>
+
+#include "dds/dds.h"
+#include "messages.h"
+
+// ### GLOBAL CONSTANTS ###
+/* An array of one message (aka sample in dds terms) will be used. */
+#define MAX_SAMPLES 1
+#define MAX_PLAYERS 5
+
+#define WIDTH  800
+#define HEIGHT 600
+#define SQ_WIDTH 50
+// How many pixels is one step
+#define STEP 1
+
+// Player coord defined as NW corner of square
+typedef struct
+{
+    bool b_active;
+    int x;
+    int y;
+} Player;
+
+// ### GLOBAL VARIABLES ###
+int int_num_active_players = 0;
+
+// Flag set by worker thread that a player just joined
+// Must be reset to false after processing by main thread
+bool b_player_join = false;
+
+// Player ID is the array index plus 1
+// -1 = No new players
+// 0 = Invalid player ID (unable to admit new players)
+// 1~MAX_PLAYERS = Valid player IDs
+int int_new_player_id = -1;
+
+// Table to track clients
+// Init all members to 0
+static Player arr_Players[MAX_PLAYERS] = {0};
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// Check if the coords collide with other squares
+bool detect_collision(Player a_arr_players[], int a_int_arr_len, 
+						int a_int_id, int a_int_x, int a_int_y) 
+{
+	bool b_res = false;
+	// Loop through the array
+	for(int i = 0; i < a_int_arr_len; i++)
+	{
+		// Index + 1 = Player ID
+		// If a player other than the player itself is active
+		if(i != a_int_id -1 && a_arr_players[i].b_active == true)
+		{
+			// If they overlap horizontally
+			if(a_int_x <= a_arr_players[i].x + SQ_WIDTH && 
+				a_arr_players[i].x <= a_int_x + SQ_WIDTH)
+			{
+				// If they overlap vertically
+				if(a_int_y <= a_arr_players[i].y + SQ_WIDTH && 
+					a_arr_players[i].y <= a_int_y + SQ_WIDTH)
+				{
+					b_res = true;
+					break;
+				}
+			}
+		}
+	}
+	
+	return b_res;
+}
+
+void *worker(void *arg)
+{
+	dds_entity_t participant;
+	dds_entity_t join_topic;
+	dds_entity_t response_topic;
+	dds_entity_t position_topic;
+	dds_entity_t input_topic;
+	dds_entity_t join_reader;
+	dds_entity_t response_writer;
+	dds_entity_t position_writer;
+	dds_entity_t input_reader;
+		
+	//Pointer to unknown type
+	void *samples_join[MAX_SAMPLES];	
+	dds_sample_info_t infos_join[MAX_SAMPLES];
+	void *samples_input[MAX_SAMPLES];	
+	dds_sample_info_t infos_input[MAX_SAMPLES];
+	
+	dds_return_t rc;
+	
+	/* ----------------------------------------------------- */
+    /* Create participant                                    */
+    /* ----------------------------------------------------- */
+
+    participant = dds_create_participant
+	(
+        DDS_DOMAIN_DEFAULT,
+        NULL,
+        NULL
+    );
+
+    if (participant < 0)
+    {
+        printf("Failed to create participant\n");
+        //return 1;
+    }
+
+    /* ----------------------------------------------------- */
+    /* Create topics                                          */
+    /* ----------------------------------------------------- */
+
+    join_topic = dds_create_topic
+	(
+        participant,
+        &JoinRequest_desc,
+        "JoinRequest",
+        NULL,
+        NULL
+    );
+	
+	response_topic = dds_create_topic
+	(
+        participant,
+        &JoinResponse_desc,
+        "JoinResponse",
+        NULL,
+        NULL
+    );
+	
+	position_topic = dds_create_topic
+	(
+        participant,
+        &Position_desc,
+        "Position",
+        NULL,
+        NULL
+    );
+	
+	input_topic = dds_create_topic
+	(
+        participant,
+        &Input_desc,
+        "Input",
+        NULL,
+        NULL
+    );
+	/* ----------------------------------------------------- */
+    /* Create readers                                         */
+    /* ----------------------------------------------------- */
+
+    join_reader = dds_create_reader(
+        participant,
+        join_topic,
+        NULL,
+        NULL
+    );
+	
+	input_reader = dds_create_reader(
+        participant,
+        input_topic,
+        NULL,
+        NULL
+    );
+	
+	 /* ----------------------------------------------------- */
+    /* Create writers                                         */
+    /* ----------------------------------------------------- */
+
+    response_writer = dds_create_writer(
+        participant,
+        response_topic,
+        NULL,
+        NULL
+    );
+	
+	position_writer = dds_create_writer(
+        participant,
+        position_topic,
+        NULL,
+        NULL
+    );
+	
+	/* Initialize sample buffer, by pointing the void pointer within
+	* the buffer array to a valid sample memory location. */
+	samples_join[0] = JoinRequest__alloc();
+	samples_input[0] = Input__alloc();
+	
+	// Create a Join Response
+	JoinResponse response;
+	
+	// Create a Position
+	Position position;
+	
+	// Record the time now
+	uint64_t last_time = dds_time();	
+	/* Poll until data has been read. */
+	while (1)
+	{
+		// Process a join request
+		rc = dds_take(join_reader, samples_join, infos_join, MAX_SAMPLES, MAX_SAMPLES);
+		
+		if (rc < 0)
+			DDS_FATAL("dds_take: %s\n", dds_strretcode(-rc));
+
+		// Check for valid join request
+		if ((rc > 0) && (infos_join[0].valid_data))
+		{
+			printf("Join request received\n");
+			// Signal the main thread to check if 
+			// number of active players reached
+			pthread_mutex_lock(&mutex);
+			b_player_join = true;
+			pthread_mutex_unlock(&mutex);
+		}
+		
+		// If a new player ID has been assigned by the main thread
+		pthread_mutex_lock(&mutex);
+		int int_new_player_id_temp = int_new_player_id;
+		/* printf("int_new_player_id_temp=%d\n", int_new_player_id_temp); */
+		pthread_mutex_unlock(&mutex);
+		if (int_new_player_id_temp != -1)
+		{
+			pthread_mutex_lock(&mutex);
+			// Reset the player ID
+			int_new_player_id = -1;
+			pthread_mutex_unlock(&mutex);
+			// The player ID can be 0, ie invalid
+			response.int_player_id = int_new_player_id_temp;
+			
+			rc = dds_write(response_writer, &response);
+			if (rc != DDS_RETCODE_OK)
+			{
+				DDS_FATAL("dds_write: %s\n", dds_strretcode(-rc));
+			}
+			printf("Join response sent\n");
+			/* Polling sleep. */
+			dds_sleepfor (DDS_MSECS (20));
+		}
+		
+		// Get the current time
+		uint64_t curr_time = dds_time();
+		// Get the time difference
+		if (curr_time - last_time > DDS_SECS(0.1))
+		{
+			// Update the last time
+			last_time = curr_time;
+			
+			// Loop through the players list
+			for(int i = 0; i < MAX_PLAYERS; i++)
+			{
+				pthread_mutex_lock(&mutex);
+				// If player is active
+				if(arr_Players[i].b_active == true)
+				{
+					position.int_player_id = i+1;
+					position.int_x = arr_Players[i].x;
+					position.int_y = arr_Players[i].y;
+					rc = dds_write(position_writer, &position);
+					if (rc != DDS_RETCODE_OK)
+					{
+						DDS_FATAL("dds_write: %s\n", dds_strretcode(-rc));
+					}
+					printf("Position sent\n");
+					/* Polling sleep. */
+					dds_sleepfor (DDS_MSECS (20));
+				}
+				pthread_mutex_unlock(&mutex);
+			}
+		}
+		
+		// Listen for input
+		rc = dds_take(input_reader, samples_input, infos_input, MAX_SAMPLES, MAX_SAMPLES);
+		
+		if (rc < 0)
+			DDS_FATAL("dds_take: %s\n", dds_strretcode(-rc));
+
+		// Check if we read some data and it is valid.
+		if ((rc > 0) && (infos_input[0].valid_data))
+		{
+			printf("Input received\n");
+			Input *input = (Input *) samples_input[0];
+			// Get the input player ID
+			int int_input_player_id = input->int_player_id;
+			// Get the input direction
+			char ch_input_dir = input->ch_direction;
+			/* printf("%d=%c\n", int_input_player_id, ch_input_dir); */
+			
+			// Get the player's current coords
+			pthread_mutex_lock(&mutex);
+			// Index is player ID minus 1
+			int int_x = arr_Players[int_input_player_id-1].x;
+			int int_y = arr_Players[int_input_player_id-1].y;
+			pthread_mutex_unlock(&mutex);
+			
+			/* printf("%d,%d\n", int_x, int_y); */
+						
+			switch (ch_input_dir)
+			{
+				case 'u':
+					int_y = int_y - STEP;
+					break;
+				case 'd':
+					int_y = int_y + STEP;
+					break;
+				case 'l':
+					int_x = int_x - STEP;
+					break;
+				case 'r':
+					int_x = int_x + STEP;
+					break;
+				
+				default:
+					break;
+			}
+			
+			// printf("%d,%d\n", int_x, int_y);
+			// printf("===========\n");
+			
+			// Check collision with window
+			if(int_x >= 0 && int_x <= WIDTH-SQ_WIDTH)
+			{
+				if(int_y >= 0 && int_y <= HEIGHT-SQ_WIDTH)
+				{
+					// printf("%d,%d\n", int_x, int_y);
+					// printf("===========\n");
+					// Check collision with other players
+					pthread_mutex_lock(&mutex);
+					// Index is player ID minus 1
+					bool b_res = detect_collision(arr_Players,
+						MAX_PLAYERS, int_input_player_id, int_x, int_y);
+					pthread_mutex_unlock(&mutex);
+					// If no collision detected
+					if(b_res == false)
+					{
+						//Update that player's position
+						pthread_mutex_lock(&mutex);
+						arr_Players[int_input_player_id-1].x = int_x;
+						arr_Players[int_input_player_id-1].y = int_y;
+						pthread_mutex_unlock(&mutex);
+					}
+				}
+			}
+			/* printf("input->int_player_id = %d\n", input->int_player_id);
+			printf("input->dir = %c\n", input->ch_direction); */
+		}
+	}
+}
+
+int main(void)
+{
+	// Seed rand(), so that the random numbers are different for each run
+	srand((unsigned int)time(NULL));
+	
+	pthread_t thread;
+
+    if (pthread_create(&thread, NULL, worker, NULL) != 0)
+    {
+        perror("pthread_create");
+        return 1;
+    }
+	
+	printf("Waiting...\n");
+	while (1)
+	{
+		// for(int i = 0; i < MAX_PLAYERS; i++)
+		// {
+			//printf("%d %d %d\n",i,arr_Players[i].x, arr_Players[i].y);
+		// }
+		//printf("============\n");
+		/* dds_sleepfor (DDS_SECS (0.5)); */
+					
+		// If a join request has been received
+		pthread_mutex_lock(&mutex);
+		bool b_player_join_temp = b_player_join;
+		pthread_mutex_unlock(&mutex);
+		if (b_player_join_temp == true)
+		{
+			// printf("b_player_join_temp\n");
+			// Reset the flag
+			pthread_mutex_lock(&mutex);
+			b_player_join = false;
+			pthread_mutex_unlock(&mutex);
+			
+			// If max active players reached
+			if (int_num_active_players == MAX_PLAYERS)
+			{
+				// Set invalid player ID
+				pthread_mutex_lock(&mutex);
+				int_new_player_id = 0;
+				pthread_mutex_unlock(&mutex);
+			}
+			else
+			{
+				pthread_mutex_lock(&mutex);
+				// Loop through the players list
+				for(int i = 0; i < MAX_PLAYERS; i++)
+				{					
+					if(arr_Players[i].b_active == false)
+					{
+						// Set this index to active
+						arr_Players[i].b_active = true;
+						// Increment the number of active players
+						int_num_active_players++;
+						
+						// Calculate initial coords of the new player
+						int int_rand_x = 0;
+						int int_rand_y = 0;						
+						while(1)
+						{
+							int_rand_x = rand()%(WIDTH-SQ_WIDTH+1);
+							int_rand_y = rand()%(HEIGHT-SQ_WIDTH+1);
+							// Function don't have to lock/unlock mutex
+							bool b_res = detect_collision(arr_Players,
+								MAX_PLAYERS, i+1, int_rand_x, int_rand_y);
+							// If no collision detected
+							if(b_res == false)
+							{
+								break;
+							}
+						}
+						
+						// Set the coords
+						arr_Players[i].x = int_rand_x;
+						arr_Players[i].y = int_rand_y;
+						
+						// Set player ID to be the index plus 1
+						/* pthread_mutex_lock(&mutex); */
+						int_new_player_id = i+1;
+						/* printf("int_new_player_id=%d\n", int_new_player_id); */
+						/* pthread_mutex_unlock(&mutex); */
+						break;
+					}
+				}
+				pthread_mutex_unlock(&mutex);
+			}
+		}		
+	}
+	
+	return 0;
+}
